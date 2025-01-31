6868e4f3b70f3256cdd3634a2dd5536618727738
MaelValin <maelmy19@gmail.com>
2025-01-31 09:09:50 +0100

    tp2 fini

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -6,15 +6,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Lego;
 
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
-class TestController extends AbstractController
+/*class TestController extends AbstractController
 {
-    /*
+    
        La méthode que l'on veut exécuter en remplacement de la page
        par défaut de Symfony. Le nom de la méthode importe peu.
-    */
+    
     #[Route('/', name: 'home')]
 
     
@@ -42,5 +43,31 @@ class TestController extends AbstractController
     }
 
     
+}*/
+
+class LegoController extends AbstractController
+{
+    #[Route('/lego', name: 'lego')]
+
+    public function lego(): Response
+    {
+        
+        $lego = new Lego(
+             10252,
+            'La coccinelle Volkwagen',
+             'Creator Expert',
+             'Construis une réplique LEGO® Creator Expert de l\'automobile la plus populaire au monde. Ce magnifique modèle LEGO est plein de détails authentiques qui capturent le charme et la personnalité de la voiture, notamment un coloris bleu ciel, des ailes arrondies, des jantes blanches avec des enjoliveurs caractéristiques, des phares ronds et des clignotants montés sur les ailes.',
+             94.99,
+             1167,
+             'LEGO_10252_Box.png',
+             'LEGO_10252_Main.jpg'
+        );
+
+        return $this->render('lego.html.twig', [ "lego" => $lego]);
+
+            
+
+        
+    }
 }
 
