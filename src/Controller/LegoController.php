531f950eeb22f61d23ff1f131a0219934b9bfd54
donzaud <donzaudf@gmail.com>
2025-02-05 17:55:39 +0100

    Fin TP3

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -6,28 +6,49 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Lego;
+use App\Service\LegoService;
+use App\Repository\LegoCollectionRepository;
 
 class LegoController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function lego()
+    public function home(LegoService $legoService): Response 
     {
-        $lego = new Lego
-        (
-            10252, 
-            "La coccinelle Volkwagen", 
-            "Creator Expert", 
-            "Construis une réplique LEGO® Creator Expert de l'automobile la plus populaire 
-            au monde. Ce magnifique modèle LEGO est plein de détails authentiques qui capturent le 
-            charme et la personnalité de la voiture, notamment un coloris bleu ciel, des ailes arrondies, 
-            des jantes blanches avec des enjoliveurs caractéristiques, des phares ronds et des 
-            clignotants montés sur les ailes.",
-            94.99,
-            1167,
-            "LEGO_10252_Box.png",
-            "LEGO_10252_Main.jpg"
-        );
-        
-        return $this -> render('lego.html.twig', ['lego' => $lego]);
+        $legos = $legoService->getLegos();
+        return $this->render('lego.html.twig', ['legos' => $legos]);
     }
+
+        // Creator
+        #[Route('/creator', name: 'creator')]
+        public function creator(LegoService $legoService): Response 
+        {
+            $legos = $legoService->getLegosByCollection('Creator');
+            return $this->render('lego.html.twig', ['legos' => $legos]);
+        }
+
+        // Star Wars
+        #[Route('/star_wars', name: 'star_wars')]
+        public function starWars(LegoService $legoService): Response 
+        {
+            $legos = $legoService->getLegosByCollection('Star Wars');
+            return $this->render('lego.html.twig', ['legos' => $legos]);
+        }
+
+        // Creator Expert
+        #[Route('/creator_expert', name: 'creator_expert')]
+        public function creatorExpert(LegoService $legoService): Response 
+        {
+            $legos = $legoService->getLegosByCollection('Creator Expert');
+            return $this->render('lego.html.twig', ['legos' => $legos]);
+        }
+
+        // test
+        #[Route('/test/{id}', 'test')]
+        public function test(int $id, LegoCollectionRepository $legoCollectionRepository): Response
+        {
+            $collection = $legoCollectionRepository->find($id);
+            dd($collection);
+        }
+    
 }
+?>
